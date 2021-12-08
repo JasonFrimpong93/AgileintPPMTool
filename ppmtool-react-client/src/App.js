10e30ca451df47679002bd385800d07f7bce8f54
JasonFrimpong93 <jason.frimpong@hcl.com>
2021-12-08 03:33:28 -0600

    Header created and Navbar

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,11 +3,13 @@ import Dashboard from "./components/Dashboard";
 import "./App.css";
 
 import React, { Component } from "react";
+import Header from "./components/Layout/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Dashboard />
       </div>
     );
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
 
 class Dashboard extends Component {
   render() {
-    return <h1>welcome to the Dashboard</h1>;
+    return (
+      <div>
+        <h1>welcome to the Dashboard</h1>;
+        <ProjectItem />
+        <ProjectItem />
+        <ProjectItem />
+      </div>
+    );
   }
 }
 
