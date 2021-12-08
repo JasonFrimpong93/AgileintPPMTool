71421c99855294f6761b80ac74431b3a848bbdfc
JasonFrimpong93 <jason.frimpong@hcl.com>
2021-12-08 03:57:04 -0600

    added bootstap

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,10 +1,8 @@
 import Dashboard from "./components/Dashboard";
-
 import "./App.css";
-
 import React, { Component } from "react";
 import Header from "./components/Layout/Header";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 class App extends Component {
   render() {
     return (
